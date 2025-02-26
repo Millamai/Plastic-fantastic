92e52c0cd9329c81c7e161a156285eea0714573d
Camilla Mai Ryskjær <CARY@zealand.dk>
2025-02-26 08:58:37 +0100

    Add project files.

---
+++ Plastic fantastic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Plastic_fantastic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
